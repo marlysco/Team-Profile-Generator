27081d3ddbc9ef00a3f8e1b92c2ed923389e79a2
marlysco <marlysco94@gmail.com>
2021-05-14 15:56:23 -0400

    Updating ths js, css and html files

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 //Variables
 const finalTeam=[];
 const questions = [
-//Personal questions 
+//Manager questions 
 //Manager's name[0]
     {
     type:"input",
@@ -22,7 +22,7 @@ const questions = [
 {
     type:"input",
     name:"email",
-    message:"Please enter the email address: "
+    message:"Please enter your email address: "
 },
 //Office Number [3]
 {
@@ -86,8 +86,8 @@ const questions = [
 
 //Main class
 class Employee {
-    constructor(tittle,name, id, email) {
-        this.tittle=tittle;
+    constructor(title,name, id, email) {
+        this.title=title;
         this.name=name;
         this.id=id;
         this.email=email;
@@ -144,10 +144,9 @@ class Intern extends Employee {
        inquirer.prompt([questions[0], questions[1], questions[2], questions[3]])
        .then(function(answer){
         const teamMaker = new Manager (answer.officeNumber, "Manager", answer.name,answer.id,answer.email);
-        console.log(teamMaker)
         //Adding the member to the team array
         finalTeam.push(teamMaker)
-        console.log(finalTeam)
+        console.log("------------------------------------------------------------------------")
         //Ask for the next member of finish the team
         addMember();
     })
@@ -185,10 +184,9 @@ class Intern extends Employee {
         inquirer.prompt([questions[4], questions[5], questions[6], questions[7]])
         .then(function(answers2){
            const engineer = new Engineer (answers2.gitHub, "Engineer", answers2.engineerName,answers2.engineerId,answers2.engineerEmail);
-           console.log(engineer)
            //Adding the member to the team array
            finalTeam.push(engineer),
-           console.log(finalTeam)
+           console.log("------------------------------------------------------------------------")
            addMember();             
     })
 }
@@ -198,16 +196,15 @@ class Intern extends Employee {
     inquirer.prompt([questions[8],questions[9], questions[10], questions[11]])
     .then(function(answers3){
        const intern = new Intern (answers3.school, "Intern", answers3.internName, answers3.internId,answers3.internEmail);
-       console.log(intern);
        //Adding the member to the team array
        finalTeam.push(intern)
-       console.log(finalTeam)
+       console.log("------------------------------------------------------------------------")
        addMember();
     })
 }
 
 function printInfo() {
-    const myteam=[]
+    const myteam=[];
     const html=`<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -215,21 +212,23 @@ function printInfo() {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>My Team</title>
+        <link rel="stylesheet" type="text/css" href="style.css" media="screen"/>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         </head>
     <body>
         <header class=""></header>
         <div class="container">
-            <div class="jumbotron">
+            <div id="header" class="jumbotron">
                 <h1 class="text-center">My Team</h1>   
             </div>
-        </div>`
+        </div>
+        <div id="main" class="container col-12">`
     myteam.push(html)
     //Adding the collected information to the html page body
-    for(let i=0; i <= finalTeam.length; i++){
-        let member = `
-        <div class="member-card color:blue">
+    for(var i=0; i < finalTeam.length; i++){
+        var member = `
+        <div id="member" class="member-card color:blue col-3 text-center">
             <div class="card-top">
              <h2>${finalTeam[i].title}</h2>
              <h2>${finalTeam[i].name}</h2>  
@@ -237,30 +236,37 @@ function printInfo() {
             <div class="card-bottom">
                 <p>Employee ID: ${finalTeam[i].id}</p>
                 <p>Email: ${finalTeam[i].email}</p>
-                <a id="email" href="mailto:${finalTeam[i].email}" class="btn btn-primary">Send email to ${finalTeam[i].name}</a>
-            <div>
+                <a href="mailto:${finalTeam[i].email}" class="btn btn-primary">Send email to ${finalTeam[i].name}</a>
+        
         ` 
         if (finalTeam[i].school) {
-            member+=`<p>School: ${(finalTeam[i].school)}</p>`
+            member+=`<p>School: ${(finalTeam[i].school)}</p>
+            </div>
+            </div> `
         };
 
         if (finalTeam[i].officeNumber) {
-            member+=`<p>Office Number: ${(finalTeam[i].officeNumber)}</p>`
+            member+=`<p>Office Number: ${(finalTeam[i].officeNumber)}</p>
+            </div>
+            </div> `
         };
 
         if (finalTeam[i].gitHub) {
-            member+=`<p>GitHub: <a href="https://github.com/${finalTeam[i].gitHub}">${finalTeam[i].gitHub}</a></p>`
+            member+=`<p>GitHub: <a href="https://github.com/${finalTeam[i].gitHub}">${finalTeam[i].gitHub}</a></p>
+            </div>
+            </div> `
            } 
-
         myteam.push(member) 
         }
-  
+        //Closing the div tag for the main container
+        member+=`</div>`
+        myteam.push(member) 
+        const myFinalTeam=myteam.join('')
 
-        fs.writeFile ("myteam.html", myteam, function(error){
+        fs.writeFile ("myteam.html", myFinalTeam, function(error){
             if(error) {
                 console.log(error)
             }
         })
 }
     
-     
